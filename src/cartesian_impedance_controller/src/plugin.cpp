9c8c5e4903d31834630c69c884fb040579299b73
Kiranraj Muthuraj <kiranrajofficial.de@yahoo.com>
2026-03-14 16:11:59 +0100

    deleted old and unused files

--- src/cartesian_impedance_controller/src/plugin.cpp
+++
@@ -1,6 +0,0 @@
-#include <pluginlib/class_list_macros.hpp>
-#include "cartesian_impedance_controller/CartesianImpedanceController.hpp"
-
-PLUGINLIB_EXPORT_CLASS(
-  cartesian_impedance_controller::CartesianImpedanceController,
-  controller_interface::ControllerInterface)
